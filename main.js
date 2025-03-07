80be6ccbf784d8f1fe101496b4eab08e081d5abb
adriano <noturnoabs@hotmail.com>
2025-03-07 18:15:02 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function scrollToContact() {
+  const contactSection = document.getElementById('contact');
+  if (contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+
+
+    
+
+     
